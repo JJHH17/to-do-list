8d1b4224dfb6a9839ca345d705ea833a421d2689
James Hatfield <james.hatfield17@icloud.com>
2025-03-12 21:18:49 +0000

    Create 'add project' modal and positoning

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./styles.css";
 
+// Stores list of projects for display
+let projects = [];
+
 // Add project button
 // Select the add project button
 function AddProjectBtnSelect() {
@@ -7,6 +10,60 @@ function AddProjectBtnSelect() {
     return addProjectBtn;
 }
 
+// Selects main div of page
+function pageContainerSelect() {
+    const pageContainer = document.querySelector("#pageContainer");
+    return pageContainer;
+}
+
+// Create modal for when button is clicked
+function createProjectModal() {
+    // Creates a parent div containing form, appends to page 
+    const containerForm = document.createElement("div");
+    containerForm.id = "containerForm";
+    // Styling and positioning 
+    containerForm.style.position = "absolute";
+    containerForm.style.left = "50%";
+    containerForm.style.right = "50%";
+    containerForm.style.top = "35%";
+
+    // Appends to page
+    pageContainerSelect().appendChild(containerForm);
+
+    // Assigns ID
+    containerForm.id = "projectFormContainer";
+    // Creates form, appends to parent
+    const projectForm = document.createElement("form");
+    projectForm.id = "projectForm";
+    containerForm.appendChild(projectForm);
+
+    // Creates title of project
+    const projectTitleLabel = document.createElement("label");
+    projectTitleLabel.textContent = "Project Title:";
+    projectForm.appendChild(projectTitleLabel);
+
+    // Creates input for title
+    const projectTitleInput = document.createElement("input");
+    projectForm.appendChild(projectTitleInput);
+
+    // Creates description of project
+    const projectDescTitle = document.createElement("label");
+    projectDescTitle.textContent = "Project Description";
+    projectForm.appendChild(projectDescTitle);
+
+    // Creates input for description
+    const projectDescInput = document.createElement("input");
+    projectForm.appendChild(projectDescInput);
+
+    // Adds submit button
+    const submitProject = document.createElement("button");
+    submitProject.type = "button";
+    submitProject.textContent = "Add";
+    projectForm.appendChild(submitProject);
+}
+
+// Handles button click
 AddProjectBtnSelect().addEventListener("click", () => {
-    alert("Hello");
+    // Creates modal on click 
+    createProjectModal();
 });
