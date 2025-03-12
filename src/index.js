d93a1a932964e15e0d4e70eb6d63e62c9363d99b
James Hatfield <james.hatfield17@icloud.com>
2025-03-12 19:10:52 +0000

    Add initial behaviour for 'Add Project' button

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import "./styles.css";
 
+// Add project button
+// Select the add project button
+function AddProjectBtnSelect() {
+    const addProjectBtn = document.querySelector("#addProject");
+    return addProjectBtn;
+}
+
+AddProjectBtnSelect().addEventListener("click", () => {
+    alert("Hello");
+});
