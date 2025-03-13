2fe6e6bd8f57f225601ff928115c96db294cb963
James Hatfield <james.hatfield17@icloud.com>
2025-03-13 20:36:25 +0000

    Create load project file, loads projects array into page

---
+++ src/sidebarRender.js
@@ -0,0 +1,28 @@
+import { AddProjectBtnSelect, pageContainerSelect, submitProjectBtnSelect, sidebarSelect } from "./elementSelectors";
+import { createProjectModal } from "./createProjectModal";
+import { createProject, projects } from "./createProject";
+
+// Responsible for rendering items to sidebar when projects are created
+// Allows user to click item, which is then expanded to center of page
+
+// Print items in project array to sidebar
+export function renderToSidebar() {
+    // Clears sidebar upon rendering
+    const sidebar = sidebarSelect();
+    sidebar.innerHTML = "";
+
+    // Loops through project array to print to sidebar
+    projects.forEach((project, index) => {
+        // Appends project to their own div
+        let projectDiv = document.createElement("div");
+        projectDiv.classList.add("projectHolder");
+        projectDiv.textContent = `${project.title} - ${project.description}`;
+        // Appends to sidebar
+        sidebar.appendChild(projectDiv);
+
+        // We'll allow divs to be clickable here and highlight when hover
+        projectDiv.addEventListener("click", () => {
+            alert("Hello");
+        })
+    });
+}
